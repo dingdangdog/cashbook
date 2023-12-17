d18b2590feff505aad9a363d49051c9687197d15
ddd <dddogx@qq.com>
2023-12-17 18:16:09 +0800

    Fix typo in Plan struct field name

--- source/server/types/Plan.go
+++ source/server/types/Plan.go
@@ -3,7 +3,7 @@ package types
 // Plan 支出计划
 type Plan struct {
 	Id         int64   `json:"id"`
-	BookId     int64   `json:"bookKey"`
+	BookId     int64   `json:"bookId"`
 	Month      string  `json:"month"`
 	LimitMoney float64 `json:"limitMoney"`
 	UsedMoney  float64 `json:"usedMoney"`
