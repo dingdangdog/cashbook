f030b001302ac44fbdab0e9eeacae9e0683b2e0a
ddd <dddogx@qq.com>
2023-12-17 17:21:20 +0800

    Remove unnecessary CSS media query

--- source/books/src/views/demo.vue
+++ source/books/src/views/demo.vue
@@ -4,12 +4,8 @@
   </div>
 </template>
 
+<script setup lang="ts">
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
