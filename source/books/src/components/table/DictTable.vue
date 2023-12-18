33adb0749a7e3600cfcb58dd16b8055d91e7f81b
ddd <dddogx@qq.com>
2023-12-18 16:48:35 +0800

    Update max-height in DictTable and FlowTable

--- source/books/src/components/table/DictTable.vue
+++ source/books/src/components/table/DictTable.vue
@@ -37,7 +37,7 @@
       :data="dictPageRef.pageData"
       stripe
       row-key="row"
-      max-height="calc(100vh - 20rem)"
+      max-height="calc(100vh - 15rem)"
     >
       <el-table-column type="index" label="序号" min-width="40" />
       <el-table-column prop="id" label="ID" v-if="false" />
--- source/books/src/components/table/FlowTable.vue
+++ source/books/src/components/table/FlowTable.vue
@@ -82,7 +82,7 @@
       @sort-change="moneySortFunc"
       stripe
       row-key="row"
-      max-height="calc(100vh - 20rem)"
+      max-height="calc(100vh - 15rem)"
     >
       <el-table-column type="index" label="序号" min-width="40" />
       <el-table-column prop="id" label="ID" v-if="false" />
