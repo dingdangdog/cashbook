e00e47da4d5e82a9781a6fa6b496c95f65200b68
ddd <dddogx@qq.com>
2023-12-18 22:55:47 +0800

    Add MainView component with router setup

---
+++ source/books/src/views/MainView.vue
@@ -0,0 +1,18 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import router from '@/router';
+import { onMounted } from 'vue';
+import { RouterView } from 'vue-router'
+
+onMounted(() => {
+  if (localStorage.getItem('token')) {
+    router.push({ path: '/index/' })
+  }
+})
+</script>
+
+<style>
+</style>
