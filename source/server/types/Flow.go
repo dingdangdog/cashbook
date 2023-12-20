c75f44a62c8b820e07775c6a491cd474f636afb2
ddd <dddogx@qq.com>
2023-12-20 15:06:46 +0800

    流失增加支出类型处理

--- source/server/types/Flow.go
+++ source/server/types/Flow.go
@@ -5,9 +5,10 @@ type Flow struct {
 	Id          int64   `json:"id"`
 	BookId      int64   `json:"bookId"`
 	Day         string  `json:"day"`
-	Type        string  `json:"type"`
+	FlowType    string  `json:"flowType"` // 流水类型：收入、支出
+	Type        string  `json:"type"`     // 流水类型：收入类型、消费类型
 	Money       float64 `json:"money"`
-	PayType     string  `json:"payType"`
+	PayType     string  `json:"payType"` // 支付方式
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 }
@@ -19,6 +20,7 @@ type FlowParam struct {
 	BookId      int64  `json:"bookId" uri:"bookId" form:"bookId"`
 	StartDay    string `json:"startDay" uri:"startDay" form:"startDay"`
 	EndDay      string `json:"endDay" uri:"endDay" form:"endDay"`
+	FlowType    string `json:"flowType" uri:"flowType" form:"flowType"` // 流水类型：收入、支出
 	Type        string `json:"type" uri:"type" form:"type"`
 	PayType     string `json:"payType" uri:"payType" form:"payType"`
 	Name        string `json:"name" uri:"name" form:"name"`
@@ -33,6 +35,7 @@ type FlowQuery struct {
 	BookId      bool
 	StartDay    bool
 	EndDay      bool
+	FlowType    bool
 	Type        bool
 	PayType     bool
 	Name        bool
