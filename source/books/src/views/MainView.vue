a3fa0aa2a0de3fe1dd13032806101b2916a93185
dddx <dingdangdogx@outlook.com>
2023-12-25 11:49:33 +0800

    add flow table dialog

--- source/books/src/views/MainView.vue
+++ source/books/src/views/MainView.vue
@@ -1,11 +1,18 @@
 <template>
   <RouterView />
+  <el-dialog v-model="showFlowTableDialog.visible" title="流水详情" width="70%" style="margin-top: 2rem !important;" :fullscreen="false">
+    <FlowTable edit="hidden"/>
+  </el-dialog>
 </template>
 
 <script setup lang="ts">
 import router from '@/router';
-import { onMounted } from 'vue';
+import { onMounted, defineAsyncComponent } from 'vue';
 import { RouterView } from 'vue-router'
+import { showFlowTableDialog } from '@/stores/flag'
+
+// 异步组件引用
+const FlowTable = defineAsyncComponent(() => import('@/components/table/FlowTable.vue'))
 
 onMounted(() => {
   if (localStorage.getItem('token')) {
