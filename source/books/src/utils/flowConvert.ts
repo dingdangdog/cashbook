b9a4698b4250f92e4388485ad264085ed813b864
dddx <dingdangdogx@outlook.com>
2024-01-13 18:37:42 +0800

    remove type suffix when alibaba flow import #20 5

--- source/books/src/utils/flowConvert.ts
+++ source/books/src/utils/flowConvert.ts
@@ -4,7 +4,8 @@ export function alipayConvert(row: any[], indexMap: Map<any, any>): Flow {
   const flow: Flow = {  }
   flow.day = row[indexMap.get('交易时间')]
   flow.flowType = row[indexMap.get('收/支')]
-  flow.type = row[indexMap.get('交易分类')] + '-ALI'
+  // + '' 防止数据不是字符串导致报错
+  flow.type = row[indexMap.get('交易分类')] + ''
   flow.payType = '支付宝'
   flow.money = row[indexMap.get('金额')]
   flow.name = row[indexMap.get('交易对方')]
