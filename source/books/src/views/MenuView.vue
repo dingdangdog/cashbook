42e0404d32534fceab52ac74b7f157ae8dd1d64e
dddx <dingdangdogx@outlook.com>
2024-02-20 17:34:15 +0800

    修改分析概览图标

--- source/books/src/views/MenuView.vue
+++ source/books/src/views/MenuView.vue
@@ -9,7 +9,7 @@
 
     <el-menu-item index="/index/analytics">
       <el-icon>
-        <Monitor />
+        <TrendCharts />
       </el-icon>
       <template #title>分析概览</template>
     </el-menu-item>
@@ -99,7 +99,7 @@ import {
   Operation,
   Sunny,
   MoonNight,
-  Monitor,
+  TrendCharts,
   EditPen,
   Reading,
   Setting,
