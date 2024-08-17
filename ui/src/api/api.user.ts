ecba1c7cab8b04748332bc4acc821591240e3cf8
dddx <dingdangdogx@outlook.com>
2024-08-17 12:55:04 +0800

    feat: 前端实现重置密码

--- ui/src/api/api.user.ts
+++ ui/src/api/api.user.ts
@@ -1,4 +1,3 @@
-
 import $http from './index'
 import type { User, LoginUser, NewPassword } from '../types/model/user'
 
@@ -7,16 +6,15 @@ import type { User, LoginUser, NewPassword } from '../types/model/user'
  * @returns Page<LoginUser>
  */
 export function login(flag: boolean, user: User): Promise<LoginUser> {
-  return $http({ url: "/login?flag=" + flag, method: "post", data: user })
+  return $http({ url: '/login?flag=' + flag, method: 'post', data: user })
 }
-
 
 /**
  * 注册
  * @returns Page<number>
  */
 export function registerApi(user: User): Promise<number> {
-  return $http({ url: "/register", method: "post", data: user })
+  return $http({ url: '/register', method: 'post', data: user })
 }
 
 /**
@@ -24,7 +22,16 @@ export function registerApi(user: User): Promise<number> {
  * @returns Page<boolean>
  */
 export function checkPassword(password: string): Promise<boolean> {
-  return $http({ url: "/admin/checkPassword/" + password, method: "post" })
+  return $http({ url: '/admin/checkPassword/' + password, method: 'post' })
+}
+
+/**
+ * 重置密码
+ * @param data
+ * @returns 
+ */
+export function resetPasswordApi(data: any): Promise<boolean> {
+  return $http({ url: '/resetPassword', method: 'post', data })
 }
 
 /**
@@ -32,21 +39,17 @@ export function checkPassword(password: string): Promise<boolean> {
  * @returns Page<boolean>
  */
 export function changePassword(newPassword: NewPassword): Promise<boolean> {
-  return $http({ url: "/admin/changePassword", method: "post", data: newPassword })
+  return $http({ url: '/admin/changePassword', method: 'post', data: newPassword })
 }
-
 
 /**
  * 修改背景图片
  * @returns Page<boolean>
  */
 export function setBackground(background: string): Promise<boolean> {
-  return $http({ url: "/admin/setBackground?background=" + background, method: "get"})
+  return $http({ url: '/admin/setBackground?background=' + background, method: 'get' })
 }
 
-
-export function checkUser(): Promise<{user:string,book:string}> {
-  return $http({ url: "/admin/checkUser", method: "get" })
+export function checkUser(): Promise<{ user: string; book: string }> {
+  return $http({ url: '/admin/checkUser', method: 'get' })
 }
-
-
