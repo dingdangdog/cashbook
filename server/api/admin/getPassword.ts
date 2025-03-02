bd1f77cfb2fc2fea0ef5b198be22537ab30c7060
dddx <dingdangdogx@outlook.com>
2025-03-02 14:13:00 +0800

    fix: password trim #67 close

--- server/api/admin/getPassword.ts
+++ server/api/admin/getPassword.ts
@@ -2,6 +2,9 @@
 export default defineEventHandler(async (event) => {
   // const Authorization = getHeader(event, "Admin");
   const body = await readBody(event);
-  const newPassword = encryptBySHA256(body.username, body.password);
+  const newPassword = encryptBySHA256(
+    String(body.username).trim(),
+    String(body.password).trim()
+  );
   return success(newPassword);
 });
