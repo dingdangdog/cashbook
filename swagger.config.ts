4857c338206c98b9f4efa13f9055dfed968f8bbd
dddx <dingdangdogx@outlook.com>
2025-11-13 22:36:43 +0800

    chore: version to 4.3.8

--- swagger.config.ts
+++ swagger.config.ts
@@ -5,7 +5,7 @@ const swaggerDefinition = {
   // openapi: "3.0.0",
   info: {
     title: "Cashbook API",
-    version: "4.3.7",
+    version: "4.3.8",
     description:
       "接口文档需要手动维护，因此不能保证完全正确，请以接口实际效果为准！",
   },
